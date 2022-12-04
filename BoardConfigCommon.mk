f74ac3729648cd357a84eef1c1927f6c6fbe6b25
aarav <107787044+kangeraarav@users.noreply.github.com>
2022-12-04 10:20:16 +0530

    sm6150-common: change dir for kernel source

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,7 +80,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sweet
 TARGET_KERNEL_CLANG_COMPILE := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
